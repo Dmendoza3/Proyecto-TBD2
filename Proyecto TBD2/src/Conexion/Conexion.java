5d3352094d8c30a5a713a09877dfb40b60b1c907
Arles96 <arles.cerrato@unitec.edu>
2018-05-25 12:02:39 -0600

    Contraseña

--- Proyecto TBD2/src/Conexion/Conexion.java
+++ Proyecto TBD2/src/Conexion/Conexion.java
@@ -159,7 +159,7 @@ public class  Conexion {
     protected boolean connect(){
         try {
             String user, pass;
-            connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "LANA", "Samir1234");
+            connect = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl", "LANA", "Samir123");
             return true;
         } catch (SQLException e) {
             System.out.println(e.getMessage());
